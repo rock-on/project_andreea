1b00423bfef5de61214deda33ba5b6ad5fe5683d
Andreea Dragu <a.dragu93@gmail.com>
2019-09-23 14:48:41 +0300

    ListUsers & DeleteUser

--- client/src/_services/UserAccessService.js
+++ client/src/_services/UserAccessService.js
@@ -2,6 +2,15 @@ import HttpService from './HttpService';
 import RestConstants from './RestConstants';
 
 export default {
+    getUsers() {
+        return new Promise(function(resolve, reject) {
+          HttpService.get(RestConstants.USERS)
+            .then(result => resolve(result))
+            .catch(err => {
+              return reject(err);
+            });
+        });
+    },
     addUser: function (user) {
         return new Promise(function (resolve, reject) {
             HttpService.post(RestConstants.USERS, user)
@@ -13,4 +22,15 @@ export default {
                 });
         });
     },
+    deleteUser(id) {
+        return new Promise(function(resolve, reject) {
+          HttpService.delete(`${RestConstants.USERS}${id}`)
+            .then(result => {
+              return resolve(result);
+            })
+            .catch(err => {
+              return reject(err);
+            });
+        });
+    }
 }
